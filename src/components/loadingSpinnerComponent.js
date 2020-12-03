47170c37b39abbde42a3e9edda3d30b9886ca198
DESKTOP-QSMSQE0\diennk <trietnk01@gmail.com>
2020-12-03 21:15:50 +0700

    optimize fetch code

--- src/components/loadingSpinnerComponent.js
+++ src/components/loadingSpinnerComponent.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import { usePromiseTracker } from "react-promise-tracker";
 import Loader from "react-loader-spinner";
-export const loadingSpinnerComponent = (props)=>{
-  const {promiseInProgress} =usePromiseTracker();
-  return (){
+export const loadingSpinnerComponent = (props) => {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
     promiseInProgress && (
-      <div className="spinner"><Loader type="ThreeDots" color="#2BAD60" height="100" width="100" /></div>
+      <div className="spinner">
+        <Loader type="ThreeDots" color="#2BAD60" height="100" width="100" />
+      </div>
     )
-  }
-}
+  );
+};
+
